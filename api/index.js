e269a4794b3cbef1eadd07f4c421c1957472ca0c
Alexzzy Silas <azamukwokusilas@gmail.com>
2026-05-22 17:04:10 +0100

    remove dotenv from api/index.js

--- api/index.js
+++ api/index.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const path = require('path');
 const dns = require('dns');
 
 dns.setServers(['8.8.8.8', '1.1.1.1']);
-
-require('dotenv').config({ path: path.join(__dirname, '..', 'backend', '.env') });
 
 const app = express();
 
